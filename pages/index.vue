b8d51e40b19730ca8b29229d672b6751c955d2b2
rickykuncoro97 <rikikuncoro@gmail.com>
2021-08-03 19:15:14 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container-fluid bg-utama">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
+    <div class="row">
+      <div class="col-3 bg-white">
+        <div class="p-5">
+          <div class="d-flex justify-content-center">
+            <img :src="config.menukiri.img_user" alt="user">
+          </div>
+          <div class="d-flex justify-content-center">
+            <p class="mt-3 h5 tebaldikit mb-3">{{config.menukiri.nama}}</p>
+          </div>
+          <div class="d-flex h6 justify-content-center">
+            <p class="h-0-8 mb-2">{{config.menukiri.profesi}}</p>
+          </div>
+          <hr class="mt-0 mb-3">
+          <div class="d-flex bd-highlight mb-3 font-10pt" aria-label="Umur">
+            <div class="me-auto p-2 bd-highlight utama">Umur: </div>
+            <div class="p-2 bd-highlight">{{config.menukiri.umur}}</div>
+          </div>
+          <div class="d-flex bd-highlight mb-3 font-10pt" aria-label="Umur">
+            <div class="me-auto p-2 bd-highlight utama">Hp: </div>
+            <div class="p-2 bd-highlight">{{config.menukiri.hp}}</div>
+          </div>
+          <div class="d-flex bd-highlight mb-3 font-10pt" aria-label="Umur">
+            <div class="me-auto p-2 bd-highlight utama">Alamat: </div>
+            <div class="p-2 bd-highlight">{{config.menukiri.alamat}}</div>
+          </div>
+          <div class="d-flex bd-highlight mb-3 font-10pt" aria-label="Umur">
+            <div class="me-auto p-2 bd-highlight utama">Status: </div>
+            <div class="p-2 bd-highlight text-success" v-if="config.menukiri.status.st===0">
+              {{config.menukiri.status.name}}</div>
+            <div class="p-2 bd-highlight text-danger" v-else>{{config.menukiri.status.name}}</div>
+          </div>
+          <hr class="mt-0 mb-3">
+          <div class="d-flex justify-content-start">
+            <p class="h5 tebaldikit mb-3">Bahasa</p>
+          </div>
+          <div aria-label="Bahasa" v-for="p in config.menukiri.bahasa" :key="p.name">
+            <div class="d-flex bd-highlight font-10pt" aria-label="">
+              <div class="me-auto p-2 ps-0 bd-highlight">{{p.name}}</div>
+              <div class="p-2 bd-highlight">{{p.value}}%</div>
+            </div>
+            <hr class="new4 mt-0 mb-0" :style="'width:'+p.value+'%'">
+          </div>
+          <hr class="mt-3 mb-3">
+          <div class="d-flex justify-content-start">
+            <p class="h5 tebaldikit mb-3">Hard Skill</p>
+          </div>
+          <div aria-label="Skill" v-for="p in config.menukiri.skills" :key="p.name">
+            <div class="d-flex bd-highlight font-10pt" aria-label="">
+              <div class="me-auto p-2 ps-0 bd-highlight">{{p.name}}</div>
+              <div class="p-2 bd-highlight">{{p.value}}%</div>
+            </div>
+            <hr class="new4 mt-0 mb-0" :style="'width:'+p.value+'%'">
+          </div>
+          <hr class="mt-3 mb-3">
+          <div class="d-flex justify-content-start">
+            <p class="h5 tebaldikit mb-3">Soft Skill</p>
+          </div>
+          <div aria-label="Skill" v-for="p in config.menukiri.soft_skills" :key="p.name">
+            <div class="d-flex bd-highlight font-10pt" aria-label="">
+              <div class="me-auto p-2 ps-0 bd-highlight">{{p.name}}</div>
+              <div class="p-2 bd-highlight">{{p.value}}%</div>
+            </div>
+            <hr class="new4 mt-0 mb-0" :style="'width:'+p.value+'%'">
+          </div>
+          <hr class="mt-3 mb-3">
+          <a :href="config.menukiri.cv">
+            <button class="button-kuning">Download CV</button>
+          </a>
+        </div>
+
+      </div>
+      <div class="col-9">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import * as config from '../static/data';
+  export default {
+    data() {
+      return {
+        config
+      }
+    }
+  }
+
+</script>
---
+++ static/data.js
@@ -0,0 +1,81 @@
+export const menukiri = {
+    img_user: "https://raw.githubusercontent.com/rickykuncoro97/myportfolio.io/main/Profile.jpg",
+    nama: "Ricky Kuncoro",
+    profesi: "Front End Developer",
+    umur: "23",
+    alamat: "Delta Puspa 111",
+    hp: "087751132069",
+    status: {
+        name: "Sedang Bekerja",
+        st: 1
+        // name: "Siap Bekerja",
+        // st: 0
+    },
+    bahasa: [
+        {
+            name: "Indonesia",
+            value: 100
+        },
+        {
+            name: "Inggris",
+            value: 50
+        },
+        {
+            name: "Jawa",
+            value: 70
+        },
+    ],
+    skills: [
+        {
+            name: "HTML",
+            value: 80
+        },
+        {
+            name: "CSS",
+            value: 70
+        },
+        {
+            name: "JS",
+            value: 80
+        },
+        {
+            name: "Vue",
+            value: 80
+        },
+        {
+            name: "Nuxt JS",
+            value: 70
+        },
+        {
+            name: "PHP",
+            value: 50
+        },
+    ],
+    soft_skills: [
+        {
+            name: "Komunikasi",
+            value: 50
+        },
+        {
+            name: "Berpikir Kritis",
+            value: 70
+        },
+        {
+            name: "Kepemimpinan",
+            value: 30
+        },
+        {
+            name: "Perilaku Baik",
+            value: 90
+        },
+        {
+            name: "Kerja Tim",
+            value: 90
+        },
+        {
+            name: "Etos Kerja",
+            value: 70
+        },
+    ],
+    cv: "https://google.com"
+}
